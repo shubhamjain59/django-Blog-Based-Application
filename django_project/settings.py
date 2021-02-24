8d0eea6162929847097ce7e8d05de2e0c245be3b
sunny <sunny@thebyto.com>
2021-02-24 15:52:59 +0530

    Setting the debug true

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_BLOG_DEBUG') == 'True'
+DEBUG = (os.environ.get('DJANGO_BLOG_DEBUG') == 'True')
 # DEBUG = True
 
 ALLOWED_HOSTS = ['djangothebyto.herokuapp.com']
