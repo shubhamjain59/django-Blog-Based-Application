5fde91f746c9a60d53984f782f42112c452ab0a7
sunny <sunny@thebyto.com>
2021-02-24 13:26:40 +0530

    Added s3 directory

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_BLOG_DEBUG') == 'True'
+# DEBUG = True
 
 ALLOWED_HOSTS = ['djangothebyto.herokuapp.com']
 
